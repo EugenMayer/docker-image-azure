6b3fd2ea4f608808dd4c19cfac738603406d8764
Eugen Mayer <136934+EugenMayer@users.noreply.github.com>
2022-11-12 18:09:54 +0100

    Add from image arg for php

--- build-php-image.sh
+++ build-php-image.sh
@@ -3,7 +3,8 @@
 set -e
 
 PHP_VERSION=$1
-BUILDDIR=$2
+FROM_IMAGE=$2
+BUILDDIR=$3
 
 cd $BUILDDIR
-docker build -t eugenmayer/azure:debian-php-cli-$PHP_VERSION-buster --build-arg PHP_VERSION=$PHP_VERSION .
+docker build -t eugenmayer/azure:debian-php-cli-$PHP_VERSION-buster --build-arg PHP_VERSION=$PHP_VERSION --build-arg FROM=$FROM_IMAGE .
