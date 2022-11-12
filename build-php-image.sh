803ef8af0171079ee764514055e1955ee5986636
Eugen Mayer <136934+EugenMayer@users.noreply.github.com>
2022-11-12 18:10:46 +0100

    Use debian release instead

--- build-php-image.sh
+++ build-php-image.sh
@@ -3,8 +3,8 @@
 set -e
 
 PHP_VERSION=$1
-FROM_IMAGE=$2
+DEBIAN_RELEASE=$2
 BUILDDIR=$3
 
 cd $BUILDDIR
-docker build -t eugenmayer/azure:debian-php-cli-$PHP_VERSION-buster --build-arg PHP_VERSION=$PHP_VERSION --build-arg FROM=$FROM_IMAGE .
+docker build -t eugenmayer/azure:debian-php-cli-$PHP_VERSION-buster --build-arg PHP_VERSION=$PHP_VERSION --build-arg FROM=debian:$DEBIAN_RELEASE .
